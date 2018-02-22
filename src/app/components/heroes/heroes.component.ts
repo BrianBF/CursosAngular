d5a25e53e2cc7433a8463d48296161bde836eebd
BrianBF <ulisesbrio@gmail.com>
2018-02-22 13:26:04 -0600

    Servicio

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroesService, Heroe} from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+            //accesible solo en este componente
+            //_nombreAliasServicio    tipo
+heroes:Heroe[] = [];
 
-  constructor() { }
+
+  constructor( private _heroesService:HeroesService) {
+      console.log("constructor");
+  }
 
   ngOnInit() {
+    this.heroes =this._heroesService.getHeroes();
+
+    console.log(this.heroes);
   }
 
 }
